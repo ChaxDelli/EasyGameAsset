b4ff182b09e39f1f64fd156154fb79ff1ce54de1
ChaxDelli <cdelligos2000@gmail.com>
2015-05-12 14:27:23 +0800

    Changes

--- Assets/Assets/Script/HitMeController.cs
+++ Assets/Assets/Script/HitMeController.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class HitMeController : MonoBehaviour {
 
+	void Start (){
+
+	}
+
+	void Update (){
+
+	}
+
 	//to proceed to next level once HitMe will collide
 	void OnCollisionEnter2D (Collision2D coll)
 	{
